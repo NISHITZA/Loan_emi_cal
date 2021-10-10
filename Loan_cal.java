2282f8151fe64fb29473e8cb91784cdbdc419771
Nishith A <nishithza@gmail.com>
2021-10-10 14:30:21 +0530

    Initial commit

---
+++ Loan_cal.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+
+class Loan_cal
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("test");
+        int choice_flag = 1;
+        while(choice_flag==1){
+            System.out.println("Menu");
+            System.out.println("1.Loan");
+            System.out.println("2.EMI calculator based on load");
+            System.out.println("3.Exit");
+            System.out.println("Please enter your choice");
+        }
+    }
+}
